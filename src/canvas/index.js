b65c5d754a480a796cb6eabe94b516cb4f2e50cc
Jiraporn Chulpaibul <over_nan@hotmail.com>
2020-08-27 12:06:01 +0800

    redraw when resize and fomatting

--- src/canvas/index.js
+++ src/canvas/index.js
@@ -1,29 +1,20 @@
 import React, { useRef, useEffect, useState } from 'react';
 
-import outline from './../assets/outlineSample3.jpg';
+import outline from './../assets/outlineSample4.jpg';
 
 function Canvas(props) {
 
   const canvasRef = useRef(null)
   const contextRef = useRef(null)
   const [isDrawing, setIsDrawing] = useState(false)
-  var resizing = false
   const ERASER_RADIUS = 10;
   var image = new Image()
-  var timeOutFunctionId
   var savedDrawing
 
   useEffect(() => {
     handleResize()
     window.addEventListener('resize', () => {
-      if (!resizing) {
-        resizing = true
-        savedDrawing = canvasRef.current.toDataURL('img/svg')
-
-      }
       handleResize()
-      clearTimeout(timeOutFunctionId)
-      timeOutFunctionId = setTimeout(redraw, 100)
     })
   }, [])
 
@@ -31,53 +22,29 @@ function Canvas(props) {
   const handleResize = () => {
     const canvas = canvasRef.current
     const context = canvas.getContext("2d")
-    const canvasWidth = window.innerWidth * 0.5
-    const canvasHeight = window.innerHeight * 0.7
-
-    canvas.style.width = `${canvasWidth}px`
-    canvas.style.height = `${canvasHeight}px`
+    savedDrawing = canvasRef.current.toDataURL('img/svg')
+    var canvasSize = 0
+    if (window.innerHeight < window.innerWidth) {
+      canvasSize = window.innerHeight * 0.7
+    }
+    else {
+      canvasSize = window.innerWidth * 0.7
+    }
+    canvas.style.width = `${canvasSize}px`
+    canvas.style.height = `${canvasSize}px`
     canvas.style.border = "2px solid black"
-
-    canvas.width = canvasWidth * 5
-    canvas.height = canvasHeight * 5
+    canvas.width = canvasSize * 5
+    canvas.height = canvasSize * 5
     context.scale(5, 5)
     context.lineCap = "round"
     context.strokeStyle = "black"
-    context.lineWidth = canvasWidth * canvasHeight * 0.00001
-
+    context.lineWidth = canvasSize * canvasSize * 0.00001
     contextRef.current = context
-  }
-
-  const redraw = () => {
-    const canvasWidth = window.innerWidth * 0.5
-    const canvasHeight = window.innerHeight * 0.7
     image.src = savedDrawing
-    var ratio = 1
     image.onload = function () {
-      const widthRatio = image.width / canvasWidth
-      const heightRatio = image.height / canvasHeight
-      if (widthRatio > heightRatio) {
-        ratio = widthRatio
-      }
-      else {
-        ratio = heightRatio
-      }
-      const widthDraw = image.width / ratio
-      const heightDraw = image.height / ratio
-
-      //postion center
-      if (widthRatio > heightRatio) {
-        const yPos = canvasHeight / 2 - heightDraw / 2
-        canvasRef.current.getContext('2d').drawImage(image, 0, yPos, widthDraw, heightDraw)
-      }
-      else {
-        const xPos = canvasWidth / 2 - widthDraw / 2
-        canvasRef.current.getContext('2d').drawImage(image, xPos, 0, widthDraw, heightDraw)
-      }
+      canvasRef.current.getContext('2d').drawImage(image, 0, 0, canvasSize, canvasSize)
     }
-    resizing = false
   }
-
 
   const startDrawing = ({ nativeEvent, type }) => {
     setIsDrawing(true);
@@ -126,6 +93,7 @@ function Canvas(props) {
 
     } else {
       contextRef.current.lineTo(offsetX, offsetY);
+      contextRef.current.strokeStyle = props.currentColor
       contextRef.current.stroke();
     }
   }
@@ -159,9 +127,47 @@ function Canvas(props) {
         style={{
           touchAction: "none",
         }} />
-      <button onClick={handleClick}>Download</button>
+      {/* <button onClick={handleClick}>Download</button> */}
     </div>
   );
 }
 
 export default Canvas;
+
+
+
+
+// const redraw = () => {
+//   const canvasWidth = window.innerWidth * 0.5
+//   const canvasHeight = window.innerHeight * 0.7
+//   image.src = savedDrawing
+//   var ratio = 1
+//   image.onload = function () {
+//     const widthRatio = image.width / canvasWidth
+//     const heightRatio = image.height / canvasHeight
+//     if (widthRatio > heightRatio) {
+//       ratio = widthRatio
+//     }
+//     else {
+//       ratio = heightRatio
+//     }
+//     const widthDraw = image.width / ratio
+//     const heightDraw = image.height / ratio
+//     console.log(widthRatio, heightRatio)
+//     //postion center
+//     if (widthRatio > heightRatio) {
+//       const yPos = canvasHeight / 2 - heightDraw / 2
+//       canvasRef.current.getContext('2d').drawImage(image, 0, yPos, widthDraw, heightDraw)
+//       canvasRef.current.getContext('2d').strokeRect(0, yPos, widthDraw, heightDraw)
+//       console.log("scale width")
+//     }
+//     else {
+//       const xPos = canvasWidth / 2 - widthDraw / 2
+//       canvasRef.current.getContext('2d').drawImage(image, xPos, 0, widthDraw, heightDraw)
+//       canvasRef.current.getContext('2d').strokeRect(xPos, 0, widthDraw, heightDraw)
+//       console.log("scale height")
+//     }
+//   }
+
+//   resizing = false
+// }
--- src/draw/index.js
+++ src/draw/index.js
@@ -8,8 +8,8 @@ import Outline from './../outline'
 
 function Draw() {
 
-  const[lines, setLines] = useState([])
-  
+  const [lines, setLines] = useState([])
+
   const color = {
     LIGHTBLUE: '#00D7DF',
     RED: '#DF0000',
@@ -19,14 +19,14 @@ function Draw() {
     PURPLE: '#B311FF',
     PINK: '#FF1192',
     YELLOW: '#FFF511',
-    BLACK:'#000000',
+    BLACK: '#000000',
     ERASER: '#FFFFFF'
-  } 
+  }
 
   const [currentColor, setCurrentColor] = useState(color.BLACK);
 
 
-  const displayImage = (image) =>{
+  const displayImage = (image) => {
     const img = document.createElement("img")
     img.src = image
     document.body.appendChild(img)
@@ -35,10 +35,10 @@ function Draw() {
 
   return (
     <div id="draw">
-        <Outline/>
-        <Canvas currentColor={currentColor} lines={lines} setLines={setLines} displayImage={displayImage}/>
-        <Stationery color={color} selectColor = {setCurrentColor} />
-   </div>
+      <Outline />
+      <Canvas currentColor={currentColor} lines={lines} setLines={setLines} displayImage={displayImage} />
+      <Stationery color={color} selectColor={setCurrentColor} />
+    </div>
   );
 }
 
