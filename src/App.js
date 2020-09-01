5c0e4598145aa47736b8c95f29a92770a551987f
TanShengRong <tansr2010@gmail.com>
2020-09-01 23:55:58 +0800

    add react dom for navi

--- src/App.js
+++ src/App.js
@@ -3,13 +3,46 @@ import React from 'react';
 import './App.css';
 
 import Draw from './draw';
-import SignUp from './components/SignUp';
+import SignUpPage, { SignUpForm } from './components/SignUp';
+import SignInPage, { SignInForm } from './components/SignIn';
+
+import {
+  BrowserRouter as Router,
+  withRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
 
 
   return (
-    <SignUp />
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/draw">Draw</Link>
+          </li>
+          <li>
+            <Link to="/components/signup">Sign up</Link>
+          </li>
+          <li>
+            <Link to="/components/signin">Sign in</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+      <Route path="/components/signin" component={withRouter(SignInPage)} />
+        <Route path="/components/signup" component={withRouter(SignUpPage)} />
+        <Route path="/draw" component={withRouter(Draw)} >
+        </Route>
+      </Switch>
+    </Router>
+    // <SignUp />
   //  <Draw/>
   );
 }
