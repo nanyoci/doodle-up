f0c3501bd44a7bee8b06e955e433a0dea97e2978
Jiraporn Chulpaibul <over_nan@hotmail.com>
2020-09-06 00:13:56 +0800

    fix css

--- src/guessing/index.js
+++ src/guessing/index.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
+import Page from '../Page';
 import './index.css'
 
 
 import dino from './../assets/dino.jpg'
 import audio from './../assets/audio.svg'
 import next from './../assets/nextArrow.svg'
+
 
 function Guessing() {
 
@@ -49,38 +51,40 @@ function Guessing() {
     }
 
     return (
-        <div id="guessing">
-            <h1 id="guessTheName">Guess the Name!</h1>
-            <div id="guessingImgDiv" >
-                <img id="guessingImg" src={dino} />
-            </div>
-            <div id="options">
-                <div className="options-row">
-                    <div className="optionUnit">
-                        <img src={audio} className="audioIcon" alt="audio" />
-                        <button type="button" className={`${currentState[0].state} option`} disabled={currentState[0].state == optionState.wrong} onClick={() => submit(0)}>Lady Bug</button>
-
-                    </div>
-                    <div className="optionUnit">
-                        <img src={audio} className="audioIcon" alt="audio" />
-                        <button type="button" className={`${currentState[1].state} option`} disabled={currentState[1].state == optionState.wrong} onClick={() => submit(1)}>Scary Cat</button></div>
-
+        <Page>
+            <div id="guessing">
+                <h1 id="guessTheName">Guess the Name!</h1>
+                <div id="guessingImgDiv" >
+                    <img id="guessingImg" src={dino} />
                 </div>
-                <div className="options-row">
-                    <div className="optionUnit">
-                        <img src={audio} className="audioIcon" alt="audio" />
-                        <button type="button" className={`${currentState[2].state} option`} disabled={currentState[2].state == optionState.wrong} onClick={() => submit(2)}>Angry Bird</button>
+                <div id="options">
+                    <div className="options-row">
+                        <div className="optionUnit">
+                            <img src={audio} className="audioIcon" alt="audio" />
+                            <button type="button" className={`${currentState[0].state} option`} disabled={currentState[0].state == optionState.wrong} onClick={() => submit(0)}>Lady Bug</button>
+
+                        </div>
+                        <div className="optionUnit">
+                            <img src={audio} className="audioIcon" alt="audio" />
+                            <button type="button" className={`${currentState[1].state} option`} disabled={currentState[1].state == optionState.wrong} onClick={() => submit(1)}>Scary Cat</button></div>
 
                     </div>
-                    <div className="optionUnit">
-                        <img src={audio} className="audioIcon" alt="audio" />
-                        <button type="button" className={`${currentState[3].state} option`} disabled={currentState[3].state == optionState.correct} onClick={() => submit(3)}>Dinosaur</button>
+                    <div className="options-row">
+                        <div className="optionUnit">
+                            <img src={audio} className="audioIcon" alt="audio" />
+                            <button type="button" className={`${currentState[2].state} option`} disabled={currentState[2].state == optionState.wrong} onClick={() => submit(2)}>Angry Bird</button>
 
+                        </div>
+                        <div className="optionUnit">
+                            <img src={audio} className="audioIcon" alt="audio" />
+                            <button type="button" className={`${currentState[3].state} option`} disabled={currentState[3].state == optionState.correct} onClick={() => submit(3)}>Dinosaur</button>
+
+                        </div>
                     </div>
                 </div>
-            </div>
-            <img src={next} alt="next" id="next" />
-        </div >
+                <img src={next} alt="next" id="next" />
+            </div >
+        </Page>
     );
 }
 
