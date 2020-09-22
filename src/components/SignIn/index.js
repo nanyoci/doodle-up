16875f8cf11a09b5fda896ea5148b4274176b0f2
Jiraporn Chulpaibul <over_nan@hotmail.com>
2020-09-22 14:54:57 +0800

    Change Sign In to function and added button sound effect

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import useSound from 'use-sound';
 
 import './index.css';
@@ -12,105 +12,88 @@ const SignInPage = () => (
     {firebase => <SignInForm firebase={firebase} />}
   </FirebaseContext.Consumer>
 );
-const INITIAL_STATE = {
-  // username: '',
-  email: '',
-  password: '',
-  error: null,
-};
 
+function SignInForm(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-class SignInForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { ...INITIAL_STATE };
-  }
-
-  xs
-
-
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  onSubmit = event => {
-    // const { email, password } = this.state;
-    // console.log(email, 'credentials');
-    // event.preventDefault();
-    // this.props.firebase
-    //   .doSignInWithEmailAndPassword(email, password)
-    //   .then(() => {
-    //     alert("Account successfully logged in.");
-    //     // this.props.history.push('/components/signup');
-    //   })
-    //   // .then(authUser => {
-    //   //   this.setState({ ...INITIAL_STATE });
-    //   // })
-    //   .then(response => console.log(response))
-    //   .catch(error => {
-    //     this.setState({ error });
-    //   });
+  const [playMouseClickSound] = useSound(
+    mouseClickSound,
+    {
+      volume: 0.5
+    }
+  )
+  const onSubmit = event => {
     event.preventDefault();
-    // this.playMouseClickSound()
+    playMouseClickSound()
+    console.log(email, 'credentials');
+    event.preventDefault();
+    props.firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        alert("Account successfully logged in.");
+        // this.props.history.push('/components/signup');
+      })
+      // .then(authUser => {
+      //   this.setState({ ...INITIAL_STATE });
+      // })
+      .then(response => console.log(response))
+      .catch(error => {
+        setError(error);
+      });
+
+
 
   }
 
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  render() {
-    const {
-      username,
-      email,
-      passwordOne,
-      passwordTwo,
-      error,
-    } = this.state;
-    return (
-      <Page isMain={true}>
-        <h1>Sign In</h1>
-        <form className="auth-form" onSubmit={this.onSubmit}>
-          <div className="fill-in-form">
-            <div className="fill-in">
-              <h2 className="fill-name">Username</h2>
-              <input
-                className="input"
-                name="email"
-                value={email}
-                onChange={this.onChange}
-                type="text"
-                placeholder="Email Address"
-              />
-            </div>
-            <div className="fill-in">
-              <h2 className="fill-name">Password</h2>
-              <input
-                className="input"
-                name="password"
-                value={passwordOne}
-                onChange={this.onChange}
-                type="password"
-                placeholder="Password"
-              />
-            </div>
-            <div className="fill-in menu-buttons">
-              <button type="submit" className="btn btn-primary">Sign In</button>
-            </div>
-
+  return (
+    <Page isMain={true}>
+      <h1>Sign In</h1>
+      <form className="auth-form" onSubmit={onSubmit}>
+        <div className="fill-in-form">
+          <div className="fill-in">
+            <h2 className="fill-name">Username</h2>
+            <input
+              className="input"
+              name="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              type="text"
+              placeholder="Email Address"
+            />
+          </div>
+          <div className="fill-in">
+            <h2 className="fill-name">Password</h2>
+            <input
+              className="input"
+              name="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="menu-buttons">
             {error && <p>{error.message}</p>}
           </div>
-          {/* <input
+          <div className="fill-in menu-buttons">
+            <button type="submit" className="btn btn-primary">Sign In</button>
+          </div>
+
+
+        </div>
+        {/* <input
     name="username"
     value={username}
         onChange={this.onChange}
         type="text"
         placeholder="Username"
       /> */}
-        </form>
-      </Page>
-    );
-  }
+      </form>
+    </Page>
+  );
+
 }
 export default SignInPage;
 export { SignInPage, SignInForm };
