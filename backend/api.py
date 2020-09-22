3a9b4a01bda37bab41fe68d0eb12d93d24e2b7f6
TanShengRong <tansr2010@gmail.com>
2020-09-22 23:15:50 +0800

    add user auth
    added auth functions are mostly built in functions from firebase.auth()

--- backend/api.py
+++ backend/api.py
@@ -8,7 +8,45 @@ app.config["DEBUG"] = True
 helper = FirebaseHelper()
 
 
-# USER AUTHENTICATION
+# creates user in firebase authentication 
+# creates entry in realtime database
+@app.route('/signup', methods=['POST'])
+def sign_up():
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
+    if helper.get_user(username):
+        return "The username has been taken.", 400
+    helper.create_new_user(username, email, password)
+    return "User created.", 200
+
+# able to return a string:token that expires in 60minutes - potentially good for prototyping secure access
+@app.route('/signin', methods=['POST'])
+def sign_in():
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
+    if not helper.get_user(username):
+        return "Does not exist, please create an account.", 400
+    # return helper.sign_in_with_email_and_password(email, password)["idToken"]
+    return "Account successfully logged in", 200
+
+# /resetpassword?email=tsr@gmail.com
+@app.route('/resetpassword', methods=['GET'])
+def reset_password():
+    email = request.args.get("email")
+    returned_json = helper.send_password_reset_email(email)
+    if "email" not in returned_json:
+        return "Please re-enter your email", 400
+    return "Sucessful! Please check your email", 200
+
+# potentially checks token issue time, email
+@app.route('/accountinfo/<string:idToken>', methods=['GET'])
+def account_info(idToken):
+    returned_json = helper.get_account_info(idToken)
+    if "users" not in returned_json:
+        return "Invalid token", 400
+    return "Successful.", 200
 
 # GAME PROGRESSION ENDPOINTS
 # Save progress on every stage pass
@@ -36,14 +74,14 @@ def get_user_progress():
     return progress
 
 # USER ENDPOINTS
-@app.route('/register', methods=['POST'])
-def register_user():
-    username = request.form['username']
-    password = request.form['password']
-    if helper.get_user(username):
-        return "The username has been taken.", 400
-    helper.create_new_user(username, password)
-    return "User created.", 200
+# @app.route('/register', methods=['POST'])
+# def register_user():
+#     username = request.form['username']
+#     password = request.form['password']
+#     if helper.get_user(username):
+#         return "The username has been taken.", 400
+#     helper.create_new_user(username, password)
+#     return "User created.", 200
 
 
 @app.route('/user/<username>', methods=['GET'])
