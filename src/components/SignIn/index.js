429949a5a5e1d8624c1fd57f4330fd769e66dbaf
Jiraporn Chulpaibul <over_nan@hotmail.com>
2020-10-04 21:21:06 +0800

    Added sign up link to sign in page

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,20 +1,13 @@
 import React, { useState } from 'react';
 import useSound from 'use-sound';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { FirebaseContext } from '../Firebase';
-import { authenticateLogin } from './../../redux/ducks/auth'
+import { authenticateLogin } from './../../redux/ducks/auth';
+import { Link } from 'react-router-dom';
 
 import './index.css';
 
 import Page from '../Page';
 import buttonClickSound from '../../assets/soundFX/buttonClick.mp3';
-
-// const SignInPage = () => (
-// <FirebaseContext.Consumer>
-//   {firebase => <SignInForm firebase={firebase} />}
-// </FirebaseContext.Consumer>
-// );
 
 function SignInPage({ authenticateLogin }) {
   const [email, setEmail] = useState("");
@@ -51,46 +44,50 @@ function SignInPage({ authenticateLogin }) {
 
   return (
     <Page isMain={true}>
-      <h1>Sign In</h1>
-      <form className="auth-form" onSubmit={onSubmit}>
-        <div className="fill-in-form">
-          <div className="fill-in">
-            <h2 className="fill-name">Username</h2>
-            <input
-              className="input"
-              name="email"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-              type="text"
-              placeholder="Email Address"
-            />
+      <div className="auth-container">
+        <h1>Sign In</h1>
+        <form className="auth-form" onSubmit={onSubmit}>
+          <div className="fill-in-form">
+            <div className="fill-in">
+              <h2 className="fill-name">Username</h2>
+              <input
+                className="input"
+                name="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                type="text"
+                placeholder="Email Address"
+              />
+            </div>
+            <div className="fill-in">
+              <h2 className="fill-name">Password</h2>
+              <input
+                className="input"
+                name="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="menu-buttons">
+              {error && <p>{error.message}</p>}
+            </div>
+            <div className=" menu-buttons">
+              <button type="submit" className="btn btn-primary">Sign In</button>
+            </div>
           </div>
-          <div className="fill-in">
-            <h2 className="fill-name">Password</h2>
-            <input
-              className="input"
-              name="password"
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              type="password"
-              placeholder="Password"
-            />
-          </div>
-          <div className="menu-buttons">
-            {error && <p>{error.message}</p>}
-          </div>
-          <div className=" menu-buttons">
-            <button type="submit" className="btn btn-primary">Sign In</button>
-          </div>
-        </div>
-        {/* <input
+          {/* <input
     name="username"
     value={username}
         onChange={this.onChange}
         type="text"
         placeholder="Username"
       /> */}
-      </form>
+        </form>
+        <br></br>
+        <Link id="auth-link" to="/signup">Create an account? Sign up</Link>
+      </div>
     </Page>
   );
 
