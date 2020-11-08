e23da34b7cef4afb2f6417a992ae2c6b46c51354
rtkleong10 <rtkleong10@gmail.com>
2020-11-08 21:51:24 +0800

    [fix] Fix storybook completion test

--- src/components/storybooks/StoryCompletionPage/index.test.js
+++ src/components/storybooks/StoryCompletionPage/index.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { cleanup, act } from '@testing-library/react'
 
+import cover from './sample/cover.png';
 import StoryCompletionPage from './index.js';
 import { renderWithReduxRouter } from './../../../utils/tests';
 
@@ -19,7 +20,8 @@ afterEach(() => {
 });
 
 const story = {
-	story_title: "Three Little Pigs"
+	story_title: "Three Little Pigs",
+	cover_image: cover,
 }
 
 it('should take a snapshot', async () => {
